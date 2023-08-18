291089d787137287c172c3ec943ff9601048ea7e
Səmədov İsmət <ismetsemedov@gmail.com>
2023-08-18 18:23:18 +0400

    check

--- unvan_az/unvan_az/spiders/links.py
+++ unvan_az/unvan_az/spiders/links.py
@@ -9,6 +9,18 @@ class LinksSpider(scrapy.Spider):
                   "https://unvan.az/heyet-evi-villa?start=1",
                   "https://unvan.az/obyekt-ofis?start=1",
                   "https://unvan.az/torpaq-sahesi?start=1"]
+    script = '''
+           function main(splash, args)
+               splash.private_mode_enabled = false
+               url = args.url
+               assert(splash:go(url))
+               splash:set_viewport_full()
+               return {
+                   png = splash:png(),
+                   html = splash:html()
+               }
+           end   
+       '''
 
     def parse(self, response):
         href=response.css('holderimg').get()
